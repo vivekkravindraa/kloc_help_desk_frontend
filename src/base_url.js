2844954d0010ff5679902c4aba7ecd02e213ee43
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-03-19 19:00:14 +0530

    baseURL: Heroku`s

--- src/base_url.js
+++ src/base_url.js
@@ -1,5 +1,5 @@
 const baseURL = 'http://localhost:3001';
 // const baseURL = 'http://dd030a75.ngrok.io';
-// const baseURL = 'https://sleepy-woodland-74100.herokuapp.com';
+// const baseURL = 'https://nameless-chamber-23997.herokuapp.com';
 
 module.exports = { baseURL };
