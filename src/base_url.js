395a0484ed7cebaaa11a31708daa82d266d1a49f
hasan <hasanfarhanascii@gmail.com>
2019-04-02 16:03:03 +0530

    changed base url

--- src/base_url.js
+++ src/base_url.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://ec2-13-234-34-58.ap-south-1.compute.amazonaws.com:3000/api';
+const baseURL = 'http://ec2-13-234-34-58.ap-south-1.compute.amazonaws.com:3001/api';
 // const baseURL = 'http://6be8d205.ngrok.io';
 // const baseURL = 'https://nameless-chamber-23997.herokuapp.com';
 
