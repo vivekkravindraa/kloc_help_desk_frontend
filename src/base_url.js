3eb2fbbc88ac641d2c9d6bfa389d130daeb40d19
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-04-17 10:24:34 +0530

    AWS Backend Base URL: Added

--- src/base_url.js
+++ src/base_url.js
@@ -1,6 +1,6 @@
-const baseURL = 'http://ec2-13-234-34-58.ap-south-1.compute.amazonaws.com:3001/api';
-// const baseURL = 'http://localhost:3001';
-// const baseURL = 'http://6be8d205.ngrok.io';
-// const baseURL = 'https://nameless-chamber-23997.herokuapp.com';
+const baseURL = 'http://localhost:3001'; // LOCAL
+// const baseURL = 'http://6be8d205.ngrok.io';  // NGROK
+// const baseURL = 'https://nameless-chamber-23997.herokuapp.com';  // HEROKU
+// const baseURL = 'http://ec2-13-234-34-58.ap-south-1.compute.amazonaws.com:3001/api'; // AWS
 
 module.exports = { baseURL };
