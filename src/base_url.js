67006ce7d3f0bc936f4500dbe8786893425c208e
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-08-19 14:31:03 +0530

    Backend URL

--- src/base_url.js
+++ src/base_url.js
@@ -1,5 +1,5 @@
 // const baseURL = 'http://localhost:3001'; // LOCAL
-const baseURL = 'http://helpdeskbackend.klocapps.com'; // PRODUCTION REPO BACKEND URL
+const baseURL = 'http://helpdeskbackend.klocapps.com/'; // PRODUCTION REPO BACKEND URL
 // const baseURL = 'http://6be8d205.ngrok.io';  // NGROK
 // const baseURL = 'https://nameless-chamber-23997.herokuapp.com';  // HEROKU
 // const baseURL = 'http://ec2-13-234-34-58.ap-south-1.compute.amazonaws.com:3001/api'; // AWS
